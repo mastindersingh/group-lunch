f52c546dd6d8d039df7a3147d988b6b73ceef816
Matt Wood <m.wood0904@gmail.com>
2017-07-04 10:36:57 -0500

    Moved chip data to JS

--- js/app.js
+++ js/app.js
@@ -1,3 +1,63 @@
+var chipContainer = document.getElementById('chip-container');
+
+
+var chipData = [
+	{ 
+		cuisine: "Diners",
+		voteCount: 0
+	},
+	{ 
+		cuisine: "Greek",
+		voteCount: 0
+	},
+	{ 
+		cuisine: "Indian",
+		voteCount: 0
+	},
+	{ 
+		cuisine: "Italian",
+		voteCount: 0
+	},
+	{ 
+		cuisine: "Korean",
+		voteCount: 0
+	},
+	{ 
+		cuisine: "Mexican",
+		voteCount: 0
+	},
+	{ 
+		cuisine: "Middle Eastern",
+		voteCount: 3
+	},
+	{ 
+		cuisine: "Pizza",
+		voteCount: 0
+	},
+	{ 
+		cuisine: "Japanese",
+		voteCount: 0
+	},
+	{ 
+		cuisine: "Thai",
+		voteCount: 2
+	},
+]
+
+for (i = 0; i < chipData.length; i++) {
+
+	var cuisine = chipData[i].cuisine;
+	var voteCount = chipData[i].voteCount;
+
+	var chipTemplate = `<div class="chip"><span class="vote-count">${voteCount}</span>${cuisine}</div>`;
+
+	chipContainer.innerHTML += chipTemplate;
+}
+
+
+
+
+
 $('.chip').click(function(e) {
   // var el = parseInt($('.vote-count').text());
 
