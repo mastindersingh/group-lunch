843c38fdb028f67176df3f2c00ddb06ab8169132
Matt Wood <m.wood0904@gmail.com>
2017-07-13 18:52:29 -0500

    Fixed help overlay

--- js/app.js
+++ js/app.js
@@ -1,111 +1,102 @@
 var chipContainer = document.getElementById('chip-container');
 var chipResultsContainer = document.getElementById("chip-container-results")
 
-
-var chipData = [
-	{ 
-		cuisine: "Diners",
-		voteCount: 0
-	},
-	{ 
-		cuisine: "Greek",
-		voteCount: 0
-	},
-	{ 
-		cuisine: "Indian",
-		voteCount: 0
-	},
-	{ 
-		cuisine: "Italian",
-		voteCount: 0
-	},
-	{ 
-		cuisine: "Korean",
-		voteCount: 0
-	},
-	{ 
-		cuisine: "Mexican",
-		voteCount: 0
-	},
-	{ 
-		cuisine: "Middle Eastern",
-		voteCount: 3
-	},
-	{ 
-		cuisine: "Pizza",
-		voteCount: 0
-	},
-	{ 
-		cuisine: "Japanese",
-		voteCount: 0
-	},
-	{ 
-		cuisine: "Thai",
-		voteCount: 2
-	},
-]
+var chipData = [{
+    cuisine: "Diners",
+    voteCount: 0
+}, {
+    cuisine: "Greek",
+    voteCount: 0
+}, {
+    cuisine: "Indian",
+    voteCount: 0
+}, {
+    cuisine: "Italian",
+    voteCount: 0
+}, {
+    cuisine: "Korean",
+    voteCount: 0
+}, {
+    cuisine: "Mexican",
+    voteCount: 0
+}, {
+    cuisine: "Middle Eastern",
+    voteCount: 3
+}, {
+    cuisine: "Pizza",
+    voteCount: 0
+}, {
+    cuisine: "Japanese",
+    voteCount: 0
+}, {
+    cuisine: "Thai",
+    voteCount: 2
+}, ]
 
 function displayChips() {
-	chipContainer.innerHTML = "";
+    chipContainer.innerHTML = "";
 
-	chipData.sort(function(a, b) {
-		// console.log(parseFloat(a.voteCount) - parseFloat(b.voteCount))
-		return parseFloat(b.voteCount) - parseFloat(a.voteCount);
-	});
+    chipData.sort(function (a, b) {
+        // console.log(parseFloat(a.voteCount) - parseFloat(b.voteCount))
+        return parseFloat(b.voteCount) - parseFloat(a.voteCount);
+    });
 
-	for (i = 0; i < chipData.length; i++) {
-		var cuisine = chipData[i].cuisine;
-		var voteCount = chipData[i].voteCount;
+    for (i = 0; i < chipData.length; i++) {
+        var cuisine = chipData[i].cuisine;
+        var voteCount = chipData[i].voteCount;
 
-		var chipTemplate = `<div class="chip"><span class="vote-count">${voteCount}</span><span class="cuisine-name">${cuisine}</span></div>`;
+        var chipTemplate = `<div class="chip"><span class="vote-count">${voteCount}</span><span class="cuisine-name">${cuisine}</span></div>`;
 
-		chipContainer.innerHTML += chipTemplate;
-	}
+        chipContainer.innerHTML += chipTemplate;
+    }
 
-	chipClick();
+    chipClick();
 }
 
 function displayChipsResults() {
-	chipResultsContainer.innerHTML = "";
+    chipResultsContainer.innerHTML = "";
 
-	chipData.sort(function(a, b) {
-		// console.log(parseFloat(a.voteCount) - parseFloat(b.voteCount))
-		return parseFloat(b.voteCount) - parseFloat(a.voteCount);
-	});
+    chipData.sort(function (a, b) {
+        // console.log(parseFloat(a.voteCount) - parseFloat(b.voteCount))
+        return parseFloat(b.voteCount) - parseFloat(a.voteCount);
+    });
 
-	for (i = 0; i < chipData.length; i++) {
-		var cuisine = chipData[i].cuisine;
-		var voteCount = chipData[i].voteCount;
-		var userZip = document.getElementById('zip-input').value;
+    for (i = 0; i < chipData.length; i++) {
+        var cuisine = chipData[i].cuisine;
+        var voteCount = chipData[i].voteCount;
+        var userZip = document.getElementById('zip-input')
+            .value;
 
-		var chipTemplate = `<a href="https://www.yelp.com/search?find_desc=${encodeURI(cuisine)}&find_loc=${userZip}&start=0&sortby=rating"><div class="chip"><span class="vote-count">${voteCount}</span><span class="cuisine-name">${cuisine}</span></div></a>`;
+        var chipTemplate = `<a href="https://www.yelp.com/search?find_desc=${encodeURI(cuisine)}&find_loc=${userZip}&start=0&sortby=rating"><div class="chip"><span class="vote-count">${voteCount}</span><span class="cuisine-name">${cuisine}</span></div></a>`;
 
-		chipResultsContainer.innerHTML += chipTemplate;
-	}
+        chipResultsContainer.innerHTML += chipTemplate;
+    }
 
-	// chipClick();
+    // chipClick();
 }
 
 displayChips();
 
-
 function chipClick() {
-$('.chip').click(function(e) {
-	cuisineName = $(this).children('.cuisine-name').html();
+    $('.chip')
+        .click(function (e) {
+            cuisineName = $(this)
+                .children('.cuisine-name')
+                .html();
 
-	currentCuisine = chipData.filter(function(obj) {
-		return obj.cuisine == cuisineName;
-	});
+            currentCuisine = chipData.filter(function (obj) {
+                return obj.cuisine == cuisineName;
+            });
 
-	// console.log(cuisineName);
-	// console.log(currentCuisine);
+            // console.log(cuisineName);
+            // console.log(currentCuisine);
 
-	currentCuisine[0].voteCount += 1;
+            currentCuisine[0].voteCount += 1;
 
-	displayChips();
-});
+            displayChips();
+        });
 };
 
-chipData.sort(function(a, b) {
-	return parseFloat(a.voteCount) - parseFloat(b.voteCount);
+chipData.sort(function (a, b) {
+    return parseFloat(a.voteCount) - parseFloat(b.voteCount);
 });
